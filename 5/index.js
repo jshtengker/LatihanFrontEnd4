9493298fbe5dc7e676c32cbcbd1774f638cedc6e
jshtengker <leonardotengker@gmail.com>
2023-10-02 22:11:22 +0800

    adding new folders

---
+++ 5/index.js
@@ -0,0 +1,31 @@
+// declaring variable let & const
+
+// let firstName = "test";  //string
+// let age = 30;   //number
+// let grade = 98.9;    //number
+// let isActive = false;    //boolean
+// let number; //undefined
+
+
+// console.log(firstName);
+// console.log(age);
+// console.log(grade);
+// console.log(isActive);
+// console.log(number);
+
+// let firstName = 'satu';
+// let lastName = 'dua';
+
+// console.log(firstName + " " + lastName);
+
+// function isNumber(number){
+//     if (number < 0){
+//         console.log(number, 'bilangan negatif');
+//     }
+//     else{
+//         console.log(number, 'bilangan positif')
+//     }      
+// }
+
+// isNumber(-2);
+// isNumber(5);
